6f947595491c5d7758176c8ba37626e838926f56
Andy-Chow-ICD2O1-02 <andy.chow2@student.tdsb.on.ca>
2024-09-23 19:01:44 +0000

    finished assignment

--- PP1_5.py
+++ PP1_5.py
@@ -1,24 +1,49 @@
 
+'''
+    Assignment: typecasting
+    Author: Andy Chow
+    Date Created: Sept 23, 2024
+    Date Last Modified: Sept 23, 2024
+'''
+
 def q1():
+    num1 = input("Input an integer: ")
+    num1 = int(num1)
+    print(num1+3)
   #Write Assignment code here
 
 def q2():
+    num2 = input("Input a number: ")
+    num2 = (str(num2)+"4")
+    num2 = (float(num2))
+    print(num2+2)
   #Write Assignment code here
 
 def q3():
+    num3 = input("Input a radius: ")
+    num3 = (float(num3))
+    print(3.14*num3**2)
   #Write Assignment code here
 
 def q4():
+    num4 = input("Input a number: ")
+    num4 = (float(num4))
+    num4 = (num4*12)
+    num4 = (int(num4))
+    print(num4)
   #Write Assignment code here
 
 def q5():
+  num5 = input("Input an integer: ")
+  num5 = (int(num5))
+  print(f"Your number + 5 is {num5+5}")
   #Write Assignment code here
 
 #Comment this code out when running tests
 #Do not comment this out when running your program normally
 
-q1()
-q2()
-q3()
-q4()
-q5()
+# q1()
+# q2()
+# q3()
+# q4()
+# q5()
